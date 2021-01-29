b262feea5cad30593b44e7095be86c137172bde9
judyal-taweel <judybataineh@gmail.com>
2021-01-29 20:13:59 +0200

    loops

--- lab4.js
+++ lab4.js
@@ -6,6 +6,25 @@ while(carType !=='bmw' && carType !=='mercedes' && carType !=='honda' && carType
   userOrder = prompt('please enter the correct item')
 }
 
+
+
+var userInputs = prompt('how many cars do you want?')
+
+var image=''; 
+
+for(var i = 0; i < userInputs; i++){
+  if(userInputs =='1' || '2' || '3' || '4'){
+    image = image + '<img src="https://saudishift.com/wp-content/uploads/2015/10/maxresdefault-6.jpg">'
+    console.log(image)
+  }else if(userInputs == '5' || '6' || '7' || userInputs >'7'){
+    image = image + '<img src="https://saudishift.com/wp-content/uploads/2017/01/0150.jpg">'
+    console.log(image)
+ }
+}
+document.write(image)
+
+
+
 var carModel= prompt("what your favorite model?");
 // var carColor= prompt("what your favorite color?");
 
@@ -26,20 +45,7 @@ document.write(orderDetails)
 document.getElementById("car").innerText=carType
 
 
-var userInputs = prompt('how many cars do you want?')
 
-var image=''; 
-
-for(var i = 0; i < userInputs; i++){
-  if(userInputs =='1' || '2' || '3' || '4'){
-    image = image + '<img src="https://saudishift.com/wp-content/uploads/2015/10/maxresdefault-6.jpg">'
-    console.log(image)
-  }else if(userInputs == '5' || '6' || '7' || userInputs >'7'){
-    image = image + '<img src="https://saudishift.com/wp-content/uploads/2017/01/0150.jpg">'
-    console.log(image)
- }
-}
-document.write(image)
 
 
 
